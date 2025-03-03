7ffcd99fe1d9e4f13068254b52952d0116de833d
sergs1305 <sergs13@gmail.com>
2025-03-03 12:24:46 +0300

    Настроит PrivateRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,6 +4,7 @@ import Login from '../../pages/login/login';
 import Favorites from '../../pages/favorites/favorites';
 import Offer from '../../pages/offer/offer';
 import Page404 from '../../pages/page404/page404';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   placesCount: number;
@@ -16,7 +17,12 @@ function App({placesCount}: AppProps) {
         <Route path="/">
           <Route index element={<Main placesCount={placesCount} />} />
           <Route path="login" element={<Login />} />
-          <Route path="favorites" element={<Favorites />} />
+          <Route path='favorites' element={
+            <PrivateRoute>
+              <Favorites />
+            </PrivateRoute>
+          }
+          />
           <Route path="offer/:id" element={<Offer />} />
           <Route path="*" element={<Page404 />} />
         </Route>
--- src/pages/page404/page404.tsx
+++ src/pages/page404/page404.tsx
@@ -1,5 +1,3 @@
-//import React from 'react';
-
 export default function Page404() {
   return <h1>Error 404. Page Not Found.</h1>;
 }

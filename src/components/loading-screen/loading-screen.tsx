1522fdfd650b1dd262f0313767d42f490986e25e
sergs1305 <sergs13@gmail.com>
2025-03-31 10:44:37 +0300

    Добавит спиннер ClockLoader из react-spinners

--- src/components/loading-screen/loading-screen.tsx
+++ src/components/loading-screen/loading-screen.tsx
@@ -1,5 +1,16 @@
+import { ClockLoader } from 'react-spinners';
+
 export default function LoadingScreen() {
   return (
-    <p>Loading ...</p>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}
+    >
+      <ClockLoader size={100} color='#4481C3' />
+    </div>
+
   );
 }

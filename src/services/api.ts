f489e98b1b8a41c92c60d937f54723d72b07b669
sergs1305 <sergs13@gmail.com>
2025-04-04 09:41:21 +0300

    Исправит ошибку билдера (уберёт явное описание типа в api.ts строка 29 (config))

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 import { getToken } from './token';
 import { StatusCodes } from 'http-status-codes';
 import { processErrorHandle } from './process-error-handle';
@@ -26,7 +26,7 @@ export const createAPI = (): AxiosInstance => {
   });
 
   api.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
+    (config) => {
       const token = getToken();
 
       if (token && config.headers) {

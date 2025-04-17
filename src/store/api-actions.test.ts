e4da6be32be125a5814af40d4e27bf546e3f9670
sergs1305 <sergs13@gmail.com>
2025-04-17 15:30:03 +0300

    Добавит тесты для асинхронных операций (api-actions)

---
+++ src/store/api-actions.test.ts
@@ -0,0 +1,113 @@
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { createAPI } from '../services/api';
+import MockAdapter from 'axios-mock-adapter';
+import thunk from 'redux-thunk';
+import { Action } from 'redux';
+import { AppThunkDispatch, extractActionsTypes, offers as mockOffers } from '../utils/mocks';
+import { State } from '../types/state';
+import { checkAuthAction, fetchOffersAction, loginAction, logoutAction } from './api-actions';
+import { APIRoute } from '../const';
+import { redirectToRoute } from './action';
+import { AuthData } from '../types/auth-data';
+
+describe('Async actions', () => {
+  const axios = createAPI();
+  const mockAxiosAdapter = new MockAdapter(axios);
+  const middleware = [thunk.withExtraArgument(axios)];
+  const mockStoreCreator = configureMockStore<State, Action<string>, AppThunkDispatch>(middleware);
+  let store: ReturnType<typeof mockStoreCreator>;
+
+  beforeEach(() => {
+    store = mockStoreCreator({ DATA: { offers: [] }});
+  });
+
+  describe('checkAuthAction', () => {
+    it('should dispatch "checkAuthAction.pending" and "checkAuthAction.fulfilled" with thunk "checkAuthAction', async () => {
+      mockAxiosAdapter.onGet(APIRoute.Login).reply(200);
+
+      await store.dispatch(checkAuthAction());
+      const actions = extractActionsTypes(store.getActions());
+
+      expect(actions).toEqual([
+        checkAuthAction.pending.type,
+        checkAuthAction.fulfilled.type,
+      ]);
+    });
+
+    it('should dispatch "checkAuthAction.pending" and "checkAuthAction.rejected" when server response 400', async() => {
+      mockAxiosAdapter.onGet(APIRoute.Login).reply(400);
+
+      await store.dispatch(checkAuthAction());
+      const actions = extractActionsTypes(store.getActions());
+
+      expect(actions).toEqual([
+        checkAuthAction.pending.type,
+        checkAuthAction.rejected.type,
+      ]);
+    });
+  });
+
+  describe('fetchOffersAction', () => {
+    it('should dispatch "fetchOffersAction.pending", "fetchOffersAction.fulfilled", when server response 200', async() => {
+      mockAxiosAdapter.onGet(APIRoute.Offers).reply(200, mockOffers);
+
+      await store.dispatch(fetchOffersAction());
+
+      const emittedActions = store.getActions();
+      const extractedActionsTypes = extractActionsTypes(emittedActions);
+      const fetchOffersActionFulfilled = emittedActions.at(1) as ReturnType<typeof fetchOffersAction.fulfilled>;
+
+      expect(extractedActionsTypes).toEqual([
+        fetchOffersAction.pending.type,
+        fetchOffersAction.fulfilled.type,
+      ]);
+
+      expect(fetchOffersActionFulfilled.payload)
+        .toEqual(mockOffers);
+    });
+
+    it('should dispatch "fetchOffersAction.pending", "fetchOffersAction.rejected" when server response 400', async () => {
+      mockAxiosAdapter.onGet(APIRoute.Offers).reply(400, []);
+
+      await store.dispatch(fetchOffersAction());
+      const actions = extractActionsTypes(store.getActions());
+
+      expect(actions).toEqual([
+        fetchOffersAction.pending.type,
+        fetchOffersAction.rejected.type,
+      ]);
+    });
+  });
+
+  describe('loginAction', () => {
+    it('should dispatch "loginAction.pending", "redirectToRoute", "loginAction.fulfilled" when server response 200', async() => {
+      const fakeUser: AuthData = { email: 'test@test.ru', password: '123456' };
+      const fakeServerReplay = { token: 'secret' };
+      mockAxiosAdapter.onPost(APIRoute.Login).reply(200, fakeServerReplay);
+
+      await store.dispatch(loginAction(fakeUser));
+      const actions = extractActionsTypes(store.getActions());
+
+      expect(actions).toEqual([
+        loginAction.pending.type,
+        redirectToRoute.type,
+        loginAction.fulfilled.type,
+      ]);
+    });
+
+    describe('logoutAction', () => {
+      it('should dispatch "logoutAction.pending", "logoutAction.fulfilled" when server response 204', async() => {
+        mockAxiosAdapter.onDelete(APIRoute.Logout).reply(204);
+
+        await store.dispatch(logoutAction());
+        const actions = extractActionsTypes(store.getActions());
+
+        expect(actions).toEqual([
+          logoutAction.pending.type,
+          logoutAction.fulfilled.type,
+        ]);
+      });
+
+    });
+  });
+});
---
+++ src/utils/mocks.ts
@@ -0,0 +1,106 @@
+// mocks for tests
+import { Offers } from '../types/types';
+import { Action } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { createAPI } from '../services/api';
+import { State } from '../types/state';
+
+export type AppThunkDispatch = ThunkDispatch<State, ReturnType<typeof createAPI>, Action>;
+
+export const extractActionsTypes = (actions: Action<string>[]) => actions.map(({ type }) => type);
+
+export const offers: Offers =
+[
+  {
+    'id': '65f052f7-32d5-4793-b672-5b7fd0808ef3',
+    'title': 'Loft Studio in the Central Area',
+    'type': 'apartment',
+    'price': 477,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/16.jpg',
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 52.36554,
+      'longitude': 4.911976,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 3.1
+  },
+  {
+    'id': '0d83c217-18d2-43dd-bcc6-dedad75c082f',
+    'title': 'Beautiful & luxurious apartment at great location',
+    'type': 'house',
+    'price': 685,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/12.jpg',
+    'city': {
+      'name': 'Cologne',
+      'location': {
+        'latitude': 50.938361,
+        'longitude': 6.959974,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 50.918461,
+      'longitude': 6.969974,
+      'zoom': 16
+    },
+    'isFavorite': true,
+    'isPremium': true,
+    'rating': 2.6
+  },
+  {
+    'id': '6cf205e5-ff4c-46ce-87ed-b5f1e4244cba',
+    'title': 'Loft Studio in the Central Area',
+    'type': 'room',
+    'price': 238,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/5.jpg',
+    'city': {
+      'name': 'Cologne',
+      'location': {
+        'latitude': 50.938361,
+        'longitude': 6.959974,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 50.957361,
+      'longitude': 6.9509739999999995,
+      'zoom': 16
+    },
+    'isFavorite': true,
+    'isPremium': false,
+    'rating': 3.7
+  },
+  {
+    'id': '445d3be5-69a6-4c9c-ab1c-b46ce0e78763',
+    'title': 'The Joshua Tree House',
+    'type': 'hotel',
+    'price': 288,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/17.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.85761,
+      'longitude': 2.358499,
+      'zoom': 16
+    },
+    'isFavorite': true,
+    'isPremium': false,
+    'rating': 3.4
+  }
+];

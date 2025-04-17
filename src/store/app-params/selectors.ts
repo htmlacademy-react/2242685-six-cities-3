59a3c5cf1dcdbc1d14e32e81b78a2e52de8da238
sergs1305 <sergs13@gmail.com>
2025-04-17 14:56:32 +0300

    Создаст тесты для slice и selectors appParams

--- src/store/app-params/selectors.ts
+++ src/store/app-params/selectors.ts
@@ -1,5 +1,5 @@
 import { NameSpace } from '../../const';
 import { State } from '../../types/state';
 
-export const getCityName = (state: State): string => state[NameSpace.App].cityName;
-export const getSortOrder = (state: State): string => state[NameSpace.App].sortOrder;
+export const getCityName = (state: Pick<State, NameSpace.App>): string => state[NameSpace.App].cityName;
+export const getSortOrder = (state: Pick<State, NameSpace.App>): string => state[NameSpace.App].sortOrder;

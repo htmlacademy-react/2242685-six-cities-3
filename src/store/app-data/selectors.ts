153992bf19c42e69b95bb7bcc5856e80fbf8aaf0
sergs1305 <sergs13@gmail.com>
2025-04-21 13:14:13 +0300

    Перенесёт логику отбора комментариев в селектор

--- src/store/app-data/selectors.ts
+++ src/store/app-data/selectors.ts
@@ -1,6 +1,9 @@
+import { nanoid } from '@reduxjs/toolkit';
 import { NameSpace } from '../../const';
 import { State } from '../../types/state';
-import { Comments, FullOffer, Offers } from '../../types/types';
+import { Comments, CommentsToDisplay, FullOffer, Offers } from '../../types/types';
+
+const MAX_COMMENTS = 10;
 
 export const getOffers = (state: State): Offers | null => state[NameSpace.Data].offers;
 export const getOffersDataLoadingStatus = (state: State): boolean => state[NameSpace.Data].isOffersDataLoading;
@@ -9,3 +12,22 @@ export const getError = (state: State): string | null => state[NameSpace.Data].e
 export const getNearOffers = (state: State): Offers | null => state[NameSpace.Data].nearOffers;
 export const getComments = (state: State): Comments | null => state[NameSpace.Data].comments;
 export const getFavorites = (state: State): Offers | null => state[NameSpace.Data].favorites;
+
+export const getFormattedComments = (state: State): CommentsToDisplay | null => {
+  const comments = getComments(state);
+
+  if (!comments || comments.length === 0) {
+    return null;
+  }
+
+  const sortedComments = [...comments].sort((a, b) =>
+    new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  return sortedComments
+    .slice(0, MAX_COMMENTS)
+    .map((comment) => ({
+      ...comment,
+      commentId: nanoid(),
+    }));
+};

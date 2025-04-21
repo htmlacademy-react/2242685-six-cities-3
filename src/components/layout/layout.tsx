4bf3a4a25e769bda4d176873fe012194b7d3e0ad
sergs1305 <sergs13@gmail.com>
2025-04-21 16:40:45 +0300

    Исправит Layout для Favorites

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './header';
+import { useAppSelector } from '../../hooks/state';
+import { getFavorites } from '../../store/app-data/selectors';
+import { Page } from '../../const';
 
 function Layout () {
+  const favorites = useAppSelector(getFavorites);
+  const location = useLocation();
+  let noFavoritesClassName = '';
+
+  if (location.pathname.includes(String(Page.Favorites))) {
+    noFavoritesClassName = favorites?.length === 0 ? 'page--favorites-empty' : '';
+  }
+
   return (
-    <div className='page'>
+    <div className={`page ${noFavoritesClassName}`}>
       <Header />
       <Outlet />
     </div>
